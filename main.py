09ba0eb917a8df0ab30698585b59a6168bea5b5e
khbash <khrystynabash09@gmail.com>
2026-02-17 14:04:26 +0200

    parameters

---
+++ main.py
@@ -0,0 +1,12 @@
+
+URL = "https://api.openweathermap.org/data/3.0/onecall"
+API_KEY = "59e915febd63afee43e6cb4897fc2f6d"
+
+PARAMS = {
+    "lat" : 50.4501,
+    "lon" : 30.5234,
+    "appid" : API_KEY,
+    "exclude" : "daily",
+    "units" : "metric",
+    "lang" : "ua"
+}
